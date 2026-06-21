f44c5955908bdc673c353eba61e7ab2943fb76d9
Italo Sanchez Manrique <italoludwings05@gmail.com>
2026-06-21 00:31:07 -0500

    Realizando las vistas de login

---
+++ lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final IconData icon;
+  final bool isPassword;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.hint,
+    required this.icon,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+        const SizedBox(height: 5),
+        TextField(
+          obscureText: isPassword,
+          decoration: InputDecoration(
+            hintText: hint,
+            prefixIcon: Icon(icon, size: 20),
+            suffixIcon: isPassword ? const Icon(Icons.visibility_off_outlined, size: 20) : null,
+            contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: Colors.grey)),
+            enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.grey.shade300)),
+          ),
+        ),
+      ],
+    );
+  }
+}
